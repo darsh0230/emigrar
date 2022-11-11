cfcd7cf09c22c0da658d514e21751680cb08391a
darsh0230 <darshss0230@gmail.com>
2022-11-11 12:31:34 +0530

    project structure setup

--- emigrar/lib/main.dart
+++ emigrar/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:emigrar/screens/home/homeScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,10 +13,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Emigrar',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.white,
       ),
-      home: Scaffold(),
+      home: HomeScreen(),
     );
   }
 }
